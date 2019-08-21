7b449b02e6ba11936f46635234649eedfc59940f
David H <iq.davidh@gmail.com>
2019-08-21 00:35:29 -0500

    avance, se ven los tabs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import 'antd/dist/antd.css';
+import './index.css';
+import { Tabs } from 'antd';
+
+const { TabPane } = Tabs;
+
+
+
+
+function callback(key) {
+  console.log(key);
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+      <Tabs defaultActiveKey="1" onChange={callback}>
+        <TabPane tab="Id Card" key="1">
+          Content of Tab Pane 1
+        </TabPane>
+        <TabPane tab="Greetings" key="2">
+          Content of Tab Pane 2
+        </TabPane>
+        <TabPane tab="Random" key="3">
+          Content of Tab Pane 3
+        </TabPane>
+        <TabPane tab="BoxColor" key="4">
+          Content of Tab Pane 3
+        </TabPane>
+
+        <TabPane tab="Card" key="6">
+          Credit Cart / DriverCard
+        </TabPane>
+
+        <TabPane tab="Rating" key="7">
+          Button
+          Rating  LikeButton ClickablePicture
+        </TabPane>
+
+
+        <TabPane tab="Dice" key="11">
+          Content of Tab Pane 3
+        </TabPane>
+
+        <TabPane tab="Carousel" key="12">
+          Content of Tab Pane 3
+        </TabPane>
+
+        <TabPane tab="NumbersTable" key="13">
+          Content of Tab Pane 3
+        </TabPane>
+
+        <TabPane tab="Facebook" key="14">
+          14
+        </TabPane>
+
+        <TabPane tab="Form" key="15">
+          15
+        </TabPane>
+
+        <TabPane tab="WRGBColorPicker" key="16">
+          16
+        </TabPane>
+
+
+      </Tabs>
+
   );
 }
 
